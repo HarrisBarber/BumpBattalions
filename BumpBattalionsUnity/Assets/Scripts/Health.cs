96ba13d67d0504fed163d6cc9b776a3d95058ef7
Harris Barber <captainotlir@gmail.com>
2019-11-15 17:20:17 -0800

    Split player unit behavior into two scripts
    Created Movement.cs which handles movment and Health.cs which handles health (doesnt do much right now)

---
+++ BumpBattalionsUnity/Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private int health;
+    [SerializeField]
+    private int maxHealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+}
---
+++ BumpBattalionsUnity/Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rigbod;
+    [SerializeField]
+    private float baseSpeed = 5.0f;
+    private float HorizontalAxis = 0.0f;
+    private float verticalAxis = 0.0f;
+    private bool canMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigbod = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetMovementAxisValues(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));//temporary to test moving around the unit
+        if (canMove)
+        {
+            rigbod.velocity = new Vector2(baseSpeed * HorizontalAxis, baseSpeed * verticalAxis);
+        }
+    }
+
+    //To be used by the player object
+    public void SetMovementAxisValues(float horizontal, float vertical)
+    {
+        float moveVectorMagnitude = Mathf.Sqrt(horizontal*horizontal + vertical*vertical);
+
+        HorizontalAxis = moveVectorMagnitude > 0 ? horizontal / moveVectorMagnitude : 0;
+        verticalAxis = moveVectorMagnitude > 0 ? vertical / moveVectorMagnitude : 0;
+    }
+
+    public void setCanMove(bool newVal)
+    {
+        canMove = newVal;
+    }
+}
