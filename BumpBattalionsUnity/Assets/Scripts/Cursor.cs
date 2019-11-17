80632f40c2cf4d4f2728da82de1129344703f875
Harris Barber <captainotlir@gmail.com>
2019-11-16 19:51:06 -0800

    Added beginnings of Cursor object

---
+++ BumpBattalionsUnity/Assets/Scripts/Cursor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject SelectUnder()
+    {
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, transform.position + new Vector3(0.0f, 0.1f));
+        if (hit)
+        {
+            return hit.collider.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
